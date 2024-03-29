d43a3b6c3a99bf20568082b41c29ef000c2d324c
PhantomWatchdog <8706@holbertonstudents.com>
2024-03-29 10:45:38 +0000

    Add digital-integer functions and prototypes

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _printf - Printf function.
+ * @format: Types detection.
+ * Return: 1 if successful.
+*/
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, j, count = 0;
+	/* Declaration of an struct array with types and functions */
+	check_t f[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'%', print_percent},
+		{'d', print_d},
+		{'i', print_i},
+		{0, NULL}
+		};
+	va_start(args, format);
+	/* Check if format is valid */
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	/* Move in format */
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%' && format[i + 1] != 'K' && format[i + 1] != '!')
+		{
+			i++;
+			/* Move in specifiers array */
+			for (j = 0; f[j].letter; j++)
+			{
+				if (f[j].letter == format[i])
+				{
+					/* Cast associated function */
+					count += f[j].f(args);
+					break;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
